f29fc905a5e31bae784960a5f4900764f73fb1c8
krstznkv <milkyyy.wayyyy@gmail.com>
2020-10-11 21:27:41 +0400

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-frontend';
+  activeTab = 'home';
+  constructor() { }
+
+  changeTab(activeTav) {
+    this.activeTab = activeTav;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { TourListComponent } from './tour-list/tour-list.component';
+
+
+const routes: Routes = [
+
+  {path: 'home', component: HomeComponent},
+  {path: 'tour-list', component: TourListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TourListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)],
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
